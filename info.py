7f022738687e1a22d20eede7bb28d3e0d15d20a4
sukhwindersingh36330-sys <sukhwindersingh36330@gmail.com>
2026-01-07 13:16:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ FILES_LIMIT = int(environ.get("FREE_FILES", "3")) #No. of File User Gets In Free
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sukhwindersingh99388:singh99@cluster0.duapykg.mongodb.net/?appName=Cluster0")
 FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "mongodb+srv://sukhwindersingh99388:singh99@cluster0.duapykg.mongodb.net/?appName=Cluster0") # For Files
-SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "mongodb+srv://sukhwindersandhu00445:sandhu80544@cluster0.bn3iigb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # 2nd DB For Files
+SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "mongodb+srv://sukhwinder088877:singh8877@cluster0.u2gw0sc.mongodb.net/?appName=Cluster0") # 2nd DB For Files
 DATABASE_NAME = environ.get('DATABASE_NAME', "Silicon")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Silicon_Files')
 
